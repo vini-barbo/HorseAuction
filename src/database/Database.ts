4a9f5c301e1b82532d066c54d383e749f425372e
Vinicius Barbosa Dos Santos <62946143+vini-barbo@users.noreply.github.com>
2024-04-25 11:33:55 -0300

    add - database connection

--- src/database/Database.ts
+++ src/database/Database.ts
@@ -1,15 +1,26 @@
-import {Client} from 'pg'
+import { Client } from 'pg'
 
 class Database {
-    public client = new Client
+    public client: Client
 
-    public async connect() {
-        console.log('teste')
+    constructor() {
+        this.client = new Client({
+            user: 'postgres',
+            host: 'localhost',
+            database: 'HorseAuction',
+            password: '2020',
+            port: 3000,
+        })
+        this.client.connect()
+        this.test()
+    }
+
+    public async test() {
         const res = await this.client.query('SELECT $1::text as message', ['Hello world!'])
-        console.log(res,'12')
+        console.log(res, '12')
         console.log(res.rows[0].message)
         await this.client.end()
     }
 }
 
-export  { Database }
+export { Database }
